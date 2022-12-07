89469c8655cd51e4e61e360eee0ba023c8768ba1
shanajasila <shanajasilam@gmail.com>
2022-12-08 00:03:09 +0530

    updated

--- src/main/java/com/example/Newshoppingcartbackend/controller/ShopingController.java
+++ src/main/java/com/example/Newshoppingcartbackend/controller/ShopingController.java
@@ -5,7 +5,9 @@ import com.example.Newshoppingcartbackend.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ShopingController {
@@ -15,14 +17,16 @@ public class ShopingController {
 
     @CrossOrigin(origins = "*")
     @PostMapping(path = "/addproduct",consumes = "application/json",produces = "application/json")
-    public String addproductpage(@RequestBody Product p){
+    public HashMap<String,String> addproductpage(@RequestBody Product p){
         System.out.println(p.getName().toString());
         System.out.println(p.getCategory());
         System.out.println(p.getDescription());
         System.out.println(p.getPrice());
         System.out.println(p.getImage());
         dao.save(p);
-        return "add product";
+        HashMap<String,String>map=new HashMap<>();
+        map.put("status","success");
+        return map;
     }
 
     @CrossOrigin(origins = "*")
