769285bcc355dfa500337ac7304b603e7ec2fc8b
shanajasila <shanajasilam@gmail.com>
2022-12-07 22:33:50 +0530

    first

---
+++ src/main/java/com/example/Newshoppingcartbackend/NewshoppingcartbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Newshoppingcartbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewshoppingcartbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewshoppingcartbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Newshoppingcartbackend/NewshoppingcartbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Newshoppingcartbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewshoppingcartbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
